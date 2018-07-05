d1ca91aa106da930024d852acf88701b4eb0d691
chengxingwei <1017802403@qq.com>
2018-07-05 12:38:25 +0800

    1

--- src/main/java/com/privilege/AppConfig.java
+++ src/main/java/com/privilege/AppConfig.java
@@ -14,7 +14,7 @@ public class AppConfig  extends  SpringBootServletInitializer{
     public  static  void main(String[] args) throws  Exception{
 //          SpringApplication.run(AppConfig.class,args);
         Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("E:\\项目资料\\PrivilegeSystem\\src\\main\\resources\\application.properties");
+        FileInputStream fileInputStream = new FileInputStream("F:\\Intellij IDEA\\PrivilegeSystem\\src\\main\\resources\\application.properties");
         properties.load(fileInputStream);
         SpringApplication app = new SpringApplication(AppConfig.class);
         app.setDefaultProperties(properties);

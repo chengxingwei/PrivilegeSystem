428ad7db91ddc4d8359e5e75b030c39e555f59f5
chengxingwei <1017802403@qq.com>
2018-07-09 14:24:40 +0800

    1

--- src/main/java/com/privilege/AppConfig.java
+++ src/main/java/com/privilege/AppConfig.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -12,7 +11,6 @@ import java.util.Properties;
 @MapperScan("com.privilege.mapper")
 public class AppConfig  extends  SpringBootServletInitializer{
     public  static  void main(String[] args) throws  Exception{
-//          SpringApplication.run(AppConfig.class,args);
         Properties properties = new Properties();
         FileInputStream fileInputStream = new FileInputStream("F:\\Intellij IDEA\\PrivilegeSystem\\src\\main\\resources\\application.properties");
         properties.load(fileInputStream);
